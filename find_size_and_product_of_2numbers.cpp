b815e61efcf96c00c1850d8895dbd0c7e5d156ac
Suhas M Angadi <93425260+Suhas0107@users.noreply.github.com>
2021-12-26 22:36:46 +0530

    created size_product.cpp
    by suhas

---
+++ find_size_and_product_of_2numbers.cpp
@@ -0,0 +1,33 @@
+// 1st program
+#include<iostream>
+using namespace std;
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  cout<<"  Size of int: "<<sizeof(int)<<endl;
+  cout<<"  Size of char: "<<  sizeof(char)<<endl;;
+  cout<<"  Size of double: "<<  sizeof(double)<<endl;
+  cout<<"  Size of  float: "<<  sizeof(float)<<endl;
+  
+  return 0;
+}
+
+
+
+
+
+//2nd Program
+#include<iostream>
+using namespace std;
+int main()
+{
+  float a,b,p;
+  cout<<" Enter two Numbers : "<<endl;
+  cin>>a>>b;
+  p=a*b;
+  cout<<"product"<<p<<endl;
+  return 0; 
+}
